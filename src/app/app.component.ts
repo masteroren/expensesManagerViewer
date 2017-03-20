9205d8fbb2120f378fa2c526048dcaac395d4fb8
orenl <A1bC$5ef!>
2017-03-20 13:22:16 +0200

    Work with node server and mySql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {HttpService} from "./shared/services/httpService";
 import {IInvoice} from "./shared/interfaces/IInvoice";
 import {DomSanitizer} from '@angular/platform-browser';
 import * as moment from 'moment';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,33 +11,34 @@ import * as moment from 'moment';
   styleUrls: ['app.component.scss'],
   providers: [HttpService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'הוצאות';
   invoices: IInvoice[];
   dt: Date = new Date();
   year: number;
   month: number;
   imageSrc: any;
+  filterType: number;
 
   constructor(private httpService: HttpService, private _domSanitizer: DomSanitizer) {
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.httpService.invoices().subscribe((response: IInvoice[]) => {
       response.forEach((item: IInvoice) => {
         item._InvoiceDate = new Date(item.createDate);
         item._InvoiceCreate = new Date(item.invoiceDate);
       });
       this.invoices = response;
-    })
+    });
   }
 
-  onDateChanged(e){
+  onDateChanged(e) {
     this.year = new Date(e).getFullYear();
-    this.month = new Date(e).getMonth()+1;
+    this.month = new Date(e).getMonth() + 1;
   }
 
-  onImageSelect(imageData: string){
+  onImageSelect(imageData: string) {
     this.imageSrc = this._domSanitizer.bypassSecurityTrustUrl('data:image/jpg;base64,' + imageData);
   }
 }
--- src/app/pipes/invoice-filter.pipe.ts
+++ src/app/pipes/invoice-filter.pipe.ts
@@ -9,11 +9,24 @@ export class InvoiceFilterPipe implements PipeTransform {
   transform(invoices: IInvoice[], args?: any): any {
     let year = args.year;
     let month = args.month;
+    let type = args.type;
 
-    if (year && month){
+    if (year && month && type){
       return invoices.filter(item=>{
-        let invoiceYear = new Date(item.createDate).getFullYear();
-        let invoiceMonth = new Date(item.createDate).getMonth()+1;
+
+        let invoiceYear: number;
+        let invoiceMonth: number;
+
+        switch (type){
+          case '0':
+            invoiceYear = new Date(item.createDate).getFullYear();
+            invoiceMonth = new Date(item.createDate).getMonth()+1;
+            break;
+          case '1':
+            invoiceYear = new Date(item.invoiceDate).getFullYear();
+            invoiceMonth = new Date(item.invoiceDate).getMonth()+1;
+            break;
+        }
 
         if (invoiceYear == year && invoiceMonth == month){
           return item;
